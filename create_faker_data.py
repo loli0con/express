01fdac13fb6f1b6fb51e306f77abf709a7098a92
loli0con@163.com <loli.xu@tec-do.com>
2021-01-12 19:15:36 +0800

    开始编写造数据脚本

---
+++ create_faker_data.py
@@ -0,0 +1,7 @@
+USER = []  # border/
+ORDER = []  # dependent: USER
+GOODS = []  # dependent: ORDER
+EVALUATION = []  # dependent: ORDER
+VEHICLE = []  # border/
+TRANSFER_STATION = []  # dependent: USER
+TRANSPORT_FRAGMENT = []  # dependent: ORDER, TRANSFER_STATION, USER, VEHICLE
