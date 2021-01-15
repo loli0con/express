202eb0a67419e5c1d0dda38d3dd1b202791ca674
loli0con@163.com <loli.xu@tec-do.com>
2021-01-16 02:31:54 +0800

    删掉不必要的查询语句

--- query_statistical_data.sql
+++ query_statistical_data.sql
@@ -1,43 +1,15 @@
--- 找到在过去一年中运送包裹最多的客户(请注意，由于测试数据是死的，所以该查询会在2021-1-30 8:00:00之后返回空结果)
+-- 找到在过去一年中运送包裹最多的客户
 SELECT O.customer_id, COUNT(1) AS '订单数量'
 FROM Order_ O
 WHERE O.create_time BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW()
-GROUP BY O.customer_id
-ORDER BY COUNT(1) DESC
-LIMIT 0,1;
-
--- 找到在过去一年中运送包裹最多的客户
--- 请注意，由于测试数据是死的，请确保NOW函数返回的时间范围在2020-1-30 8:00:00 到 2021-1-30 8:00:00 之间
-SELECT O.customer_id, COUNT(1) AS '订单数量'
-FROM Order_ O
-WHERE O.create_time BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW()
-GROUP BY O.customer_id
-ORDER BY COUNT(1) DESC
-LIMIT 0,1;
-
--- 找到在过去一年中运送包裹最多的客户
--- 该查询假定当前datetime为2021-1-1 00:00:00
-SELECT O.customer_id, COUNT(1) AS '订单数量'
-FROM Order_ O
-WHERE O.create_time BETWEEN DATE_SUB('2021-1-1 00:00:00', INTERVAL 1 YEAR) AND '2021-1-1 00:00:00'
 GROUP BY O.customer_id
 ORDER BY COUNT(1) DESC
 LIMIT 0,1;
 
 -- 找到在过去一年中花费最多的客户
--- 请注意，由于测试数据是死的，请确保NOW函数返回的时间范围在2020-1-30 8:00:00 到 2021-1-30 8:00:00 之间
 SELECT O.customer_id, SUM(O.payment) AS '花费总额'
 FROM Order_ O
 WHERE O.create_time BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW()
-GROUP BY O.customer_id
-ORDER BY SUM(O.payment) DESC
-LIMIT 0,1;
-
--- 找到在过去一年中花费最多的客户
--- 该查询假定当前datetime为2021-1-1 00:00:00
-SELECT O.customer_id, SUM(O.payment) AS '花费总额'
-FROM Order_ O
-WHERE O.create_time BETWEEN DATE_SUB('2021-1-1 00:00:00', INTERVAL 1 YEAR) AND '2021-1-1 00:00:00'
 GROUP BY O.customer_id
 ORDER BY SUM(O.payment) DESC
 LIMIT 0,1;
